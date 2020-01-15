b917978b45fb96f21b23aac2420ac8b2f7f3c954
Sing Heung <singwai0510@gmail.com>
2020-01-15 18:43:09 +0800

    heading added

--- products.py
+++ products.py
@@ -17,3 +17,8 @@ for p in products:
 
 for p in products:
 	print(p[0],'的價格是',p[1])
+
+with open('products.csv','w') as f:
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
