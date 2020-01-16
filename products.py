dc55e08316c0c3737fe7e840d79977b3a1cdd4f2
Sing Heung <singwai0510@gmail.com>
2020-01-16 14:58:48 +0800

    refactor

--- products.py
+++ products.py
@@ -1,55 +1,60 @@
 import os #operating system
 
 #讀取檔案
-products = []
-
-if os.path.isfile('products.csv'): #檢查檔案在不在
-	print('yeah! 找到檔案了解')
-	with open('products.csv', 'r') as f:
+def read_file(filename):
+	products = []
+	with open(filename, 'r') as f:
 		for line in f:
 			if '商品,價格' in line:
 				continue 
 			name, price = line.strip().split(',')
 			products.append([name, price])
-		print(products)
-else:
-    print('找不到檔案')
-
-
-with open('products.csv', 'r') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue #
-		name, price = line.strip().split(',') #name = s[0] price = s[1]
-		products.append([name,price])
-
-
-print(products)
-
+	return products
 
 #讓便用者輸入
+def user_input(products):
 
-while True:
-	name = input('請輸入商品名稱:')
-	if name == 'q':
-		break
-	price = input('請輸入商品價格:')
-	p = [name,price]
-	products.append([name,price])
-print(products)
+	while True:
+		name = input('請輸入商品名稱:')
+		if name == 'q':
+			break
+		price = input('請輸入商品價格:')
+		p = [name,price]
+		products.append([name,price])
+	return products
 
 # 列出
-for p in products:
-	print(p)
+def print_products(products):
 
-for p in products:
-	print(p[0])
+	for p in products:
+		print(p)
 
-for p in products:
-	print(p[0],'的價格是',p[1])
+	for p in products:
+		print(p[0])
+
+	for p in products:
+		print(p[0],'的價格是',p[1])
 
 #寫入檔案
-with open('products.csv','w') as f:
-	f.write('商品,價格\n')
-	for p in products:
-		f.write(p[0] + ',' + str(p[1]) + '\n')
+def write_file(filename, products):
+
+	with open(filename,'w') as f:
+		f.write('商品,價格\n')
+		for p in products:
+			f.write(p[0] + ',' + str(p[1]) + '\n')
+
+
+def main():
+	filename = 'products.csv'
+	if os.path.isfile(filename): #檢查檔案在不在
+		print('yeah! 找到檔案了解')
+		products = read_file(filename)
+	else:
+		rint('找不到檔案')
+
+	products = user_input(products)
+	print_products(products)
+	write_file('products.csv', products) #主要funtion
+
+main() #執行
+
